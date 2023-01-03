ad9e1039b8143fb4dd013e1f6df1bcd41c0a731f
Ben Jenkins <benjenkinsiv@gmail.com>
2023-01-03 02:07:48 -0600

    updates

--- create_keys.sh
+++ create_keys.sh
@@ -18,3 +18,19 @@ echo 'RSA_PUBLIC_KEY = "'$public_key'"' > RSA_PUBLIC_KEY.txt
 
 # Update the value of "snowflake.private.key" in connector_config.json
 jq --arg SNOWFLAKE_PRIVATE_KEY "$private_key" '.config |= .+{"snowflake.private.key":$SNOWFLAKE_PRIVATE_KEY}' kafka-connect-integration/snowflake-connector-sample.json > kafka-connect-integration/snowflake-connector.json
+#!/bin/bash
+
+# Read config.txt and store the values in variables
+while IFS='=' read -r key value; do
+    case "$key" in
+        "PASSPHRASE") passphrase="$value" ;;
+        "WAREHOUSE_NAME") warehouse_name="$value" ;;
+        "DATABASE_NAME") database_name="$value" ;;
+        "SCHEMA_NAME") schema_name="$value" ;;
+        "ROLE_NAME") role_name="$value" ;;
+        "USER_NAME") user_name="$value" ;;
+    esac
+done < config.txt
+
+# # Update snowflake-connector.json with the values from config.txt
+jq --arg user_name "$user_name" --arg passphrase "$passphrase" --arg database_name "$database_name" --arg schema_name "$schema_name" '.config |= . + ({snowflake.user.name: $user_name, snowflake.private.key.passphrase: $passphrase, snowflake.database.name: $database_name, snowflake.schema.name: $schema_name})' snowflake-connector-sample.json > snowflake-connector.json.tmp 
